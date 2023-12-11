f672e3c046328351b54199ce7e91a31ac028f1f0
ericli <helloeric01@gmail.com>
2023-12-11 22:12:08 +0800

    feat: main and draw of Screen

--- app/theme/colors.ts
+++ app/theme/colors.ts
@@ -1,84 +1,50 @@
-// TODO: write documentation for colors and palette in own markdown file and add links from here
-
 const palette = {
-  neutral100: "#FFFFFF",
-  neutral200: "#F4F2F1",
-  neutral300: "#D7CEC9",
-  neutral400: "#B6ACA6",
-  neutral500: "#978F8A",
-  neutral600: "#564E4A",
-  neutral700: "#3C3836",
-  neutral800: "#191015",
-  neutral900: "#000000",
+  neutral100: "#FFFFFF",  // White
+  neutral200: "#EEEEEE",
+  neutral300: "#BDBDBD",
+  neutral400: "#9E9E9E",
+  neutral500: "#757575",  // Gray
+  neutral600: "#616161",
+  neutral700: "#424242",
+  neutral800: "#212121",  // Dark Gray
+  neutral900: "#000000",  // Black
 
-  primary100: "#F4E0D9",
-  primary200: "#E8C1B4",
-  primary300: "#DDA28E",
-  primary400: "#D28468",
-  primary500: "#C76542",
-  primary600: "#A54F31",
+  primary100: "#BBDEFB",  // Light Blue
+  primary200: "#90CAF9",
+  primary300: "#64B5F6",
+  primary400: "#42A5F5",
+  primary500: "#2196F3",  // Blue
 
-  secondary100: "#DCDDE9",
-  secondary200: "#BCC0D6",
-  secondary300: "#9196B9",
-  secondary400: "#626894",
-  secondary500: "#41476E",
+  secondary100: "#CFD8DC",  // Light Blue Gray
+  secondary200: "#B0BEC5",
+  secondary300: "#90A4AE",
+  secondary400: "#78909C",
+  secondary500: "#607D8B",  // Blue Gray
 
-  accent100: "#FFEED4",
-  accent200: "#FFE1B2",
-  accent300: "#FDD495",
-  accent400: "#FBC878",
-  accent500: "#FFBB50",
+  accent100: "#D1C4E9",  // Light Purple
+  accent200: "#B39DDB",
+  accent300: "#9575CD",
+  accent400: "#7E57C2",
+  accent500: "#673AB7",  // Deep Purple
 
-  angry100: "#F2D6CD",
-  angry500: "#C03403",
+  angry100: "#EF9A9A",  // Light Red
+  angry500: "#D32F2F",  // Deep Red
 
-  overlay20: "rgba(25, 16, 21, 0.2)",
-  overlay50: "rgba(25, 16, 21, 0.5)",
-} as const
+  overlay20: "rgba(33, 150, 243, 0.2)",  // Translucent Blue
+  overlay50: "rgba(33, 150, 243, 0.5)",
+
+} as const;
 
 export const colors = {
-  /**
-   * The palette is available to use, but prefer using the name.
-   * This is only included for rare, one-off cases. Try to use
-   * semantic names as much as possible.
-   */
   palette,
-  /**
-   * A helper for making something see-thru.
-   */
   transparent: "rgba(0, 0, 0, 0)",
-  /**
-   * The default text color in many components.
-   */
-  text: palette.neutral800,
-  /**
-   * Secondary text information.
-   */
-  textDim: palette.neutral600,
-  /**
-   * The default color of the screen background.
-   */
-  background: palette.neutral200,
-  /**
-   * The default border color.
-   */
-  border: palette.neutral400,
-  /**
-   * The main tinting color.
-   */
-  tint: palette.primary500,
-  /**
-   * A subtle color used for lines.
-   */
+  text: palette.neutral100,  // White text for dark backgrounds
+  textDim: palette.neutral500,
+  background: palette.neutral800,  // Dark background
+  surface: palette.neutral700,  // For card-like surfaces
+  border: palette.neutral300,
+  tint: palette.primary500,  // Blue for interactive elements
   separator: palette.neutral300,
-  /**
-   * Error messages.
-   */
-  error: palette.angry500,
-  /**
-   * Error Background.
-   *
-   */
+  error: palette.angry500,  // For errors
   errorBackground: palette.angry100,
-}
+};
